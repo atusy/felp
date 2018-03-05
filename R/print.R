d49747ad4d348c50e8d821789124fdef0e4aef8c
atusy <30277794+atusy@users.noreply.github.com>
2018-03-06 00:12:40 +0900

    feat tries to print help because some functions do not have help fix ? instead of help in utils perf moved deparsing of input x within if because it is required only when print is called explicitly

--- R/print.R
+++ R/print.R
@@ -1,30 +1,28 @@
 #' Display source and help of function
 #' @param x function
 #' @param ... Other arguments passed to felp. felp is called when print.function is explicitly called or via print.
-#' @importFrom utils help
 #' @importFrom utils savehistory
 #' @importFrom utils tail
+#' @importFrom utils ?
 #' @export
 #'
 print.function <- function(x, ...) {
-  input <- rev(lapply(substitute(x), deparse))
-  if(!is.function(x)) stop("x must be a function")
-
-  # get latest command
   tempfile <- tempfile(pattern="rhistory_felp_", fileext=".txt")
-  savehistory(tempfile)
+  utils::savehistory(tempfile)
   cmd <- as.character(parse(text = tail(readLines(tempfile), 1)))
+  # cmd <- deparse(rev(histry::histry())[[2]])
 
   # return result of felp when print.function is explicitly called or via print.
   if(grepl('print(.function)?\\(', cmd)) {
+    input <- rev(lapply(substitute(x), deparse))
+    if(!is.function(x)) stop("x must be a function")
     return(felp(x = input[[1]], package = input[2][[1]]))
   }
 
   #print help
-  print(eval(parse(text = paste0('?', cmd))))
+  try(print(eval(parse(text = paste0('?', cmd)))))
 
   #print source
   base::print.function(eval(parse(text = cmd)))
 }
-
 
