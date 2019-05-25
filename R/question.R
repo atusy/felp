8098512b5715a4b04aeea7e5e31e86e01a7b4aee
atusy <30277794+atusy@users.noreply.github.com>
2019-05-26 02:14:33 +0900

    ? supports package?p syntax to display help of a package

--- R/question.R
+++ R/question.R
@@ -1,17 +1,16 @@
 #' Functional help with `?` operator
 #'
-#' Displays help and structure of an object.
-#' A special syntax `?.` is supported in addition to
-#' the one in `utils::?`.
-#' `?.` works as if it is a postfix operator.
-#' For example, `help?.` is equivalent to `?help`.
+#' Displays help and structure of an object, or help of a package.
+#' Two syntaxes are added to those of `utils::?`.
+#' One is `object?.` which works as if `?object`.
+#' Another is `package?p` which works as if `help(package = package)`
 #'
 #' @param e1
-#'   A topic ought to be documented.
+#'   A topic of document.
 #'   Refer to `topic` argument described in `` utils::`?` ``.
 #' @param e2
-#'   `.` equals to `missing(e2)`.
-#'   If else, see `type` argument described in `` utils::`?` ``.
+#'   `.` and `p` have special meanings as documented above.
+#'   Otherwise, `e2` is same as `type` argument of `` utils::`?` ``.
 #'
 #' @rdname question
 #' @aliases ?
@@ -21,8 +20,9 @@
 `?` <-  function(e1, e2) {
   .e1 <- substitute(e1)
   .e2 <- substitute(e2)
-  if (!missing(e2) && as.character(.e2) != ".") {
-    return(do.call(utils::`?`, list(e1 = .e1, e2 = .e2)))
-  }
-  do.call(felp, list(topic = .e1))
+  if (missing(e2)) return(do.call(felp, list(topic = .e1)))
+  .e2_chr <- as.character(.e2)
+  if (.e2_chr == ".") return(do.call(felp, list(topic = .e1)))
+  if (.e2_chr == "p") return(do.call(help, list(package = .e1)))
+  do.call(utils::`?`, list(e1 = .e1, e2 = .e2))
 }
