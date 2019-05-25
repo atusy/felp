7ce5c54912360d901a3b7d2c717e048bf26c1740
atusy <30277794+atusy@users.noreply.github.com>
2019-05-26 01:36:51 +0900

    felp returns structure of objects

--- R/felp.R
+++ R/felp.R
@@ -1,8 +1,10 @@
-#' Returns source and help of a function simultaneously
+#' Functional help which displays structure an object in addition to help
 #'
-#' @param topic A name or character string specifying the function for which help and source are sought. Package to be sought can also be defined simultaneously if x is specified in the style of package::name or package:::name
-#' @param package A name or character string specifying the package for which help and source are sought. If the package is specified by x, this parameter is neglected, or NULL (default).
-#' @param ... Other arguments passed to help
+#' Structure of object is returned by `str()`.
+#' For a function, its source is returned instead of `str()`.
+#'
+#' @inheritParams utils::help
+#' @inheritDotParams utils::help -topic -package
 #' @importFrom utils help
 #' @export
 #'
@@ -15,9 +17,7 @@ felp <- function(topic, package = NULL, ...) {
   if (is.na(p)) p <- NULL
 
   # Print source of the function
-  prettycode:::print.function(get(
-    t[1L], envir = `if`(is.null(p), parent.frame(), asNamespace(p))
-  ))
+  str(get(t[1L], envir = `if`(is.null(p), parent.frame(), asNamespace(p))))
 
   try(help(t[1L], p[1L], ...))
 
---
+++ R/str.R
@@ -0,0 +1,5 @@
+#' str for function
+#' @noRd
+str.function <- function(object, ...) {
+  prettycode:::print.function(object, ...)
+}
