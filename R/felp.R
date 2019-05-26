9a16ebe3e9c3ca48ca69b029dde4fd5004b9dac1
atusy <30277794+atusy@users.noreply.github.com>
2019-05-26 10:12:57 +0900

    felp supports displaying package documentations

--- R/felp.R
+++ R/felp.R
@@ -1,4 +1,4 @@
-#' Functional help which displays structure an object in addition to help
+#' Functional help which displays structure of an object in addition to help
 #'
 #' Structure of object is returned by `str()`.
 #' For a function, its source is returned instead of `str()`.
@@ -9,14 +9,16 @@
 #' @export
 #'
 felp <- function(topic, package = NULL, ...) {
-  # convert package::name to c("name", "package", "`::`")
-  # or name to "name"
+  # Display package document
+  if (missing(topic)) return(do.call(help, list(package = substitute(package), ...)))
+
+  # Convert `package::name` to c("name", "package", "`::`") or `name` to "name"
   t <- rev(as.character(substitute(topic)))
 
   p <- c(as.character(substitute(package)), t[2L])[1L]
   if (is.na(p)) p <- NULL
 
-  # Print source of the function
+  # Display structure and document of an object
   str(get(t[1L], envir = `if`(is.null(p), parent.frame(), asNamespace(p))))
 
   try(help(t[1L], p[1L], ...))
