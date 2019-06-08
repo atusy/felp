d1b9e8cdb1f5810fdfa1bb47a176745baa95d21d
atusy <30277794+atusy@users.noreply.github.com>
2019-06-08 15:12:15 +0900

    test: felp

--- tests/testthat/test-felp.R
+++ tests/testthat/test-felp.R
@@ -1,3 +1,9 @@
 test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+  rmarkdown::render("test-felp.Rmd", output_file =  "test-felp-ans.md", encoding = "UTF-8", quiet = TRUE)
+  tst <- readLines("test-felp.md")
+  ans <- readLines("test-felp-ans.md")
+  expect_identical(
+    tst[!grepl("^    ## <bytecode:", tst)],
+    ans[!grepl("^    ## <bytecode:", ans)]
+  )
 })
